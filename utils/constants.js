cac815876cc8ba97191bea886305bb47f29a5514
Rohit Rawat <rohit.singh.rr6@gmail.com>
2024-06-25 19:01:31 +0530

    edited other projects

--- utils/constants.js
+++ utils/constants.js
@@ -1,13 +1,13 @@
 export const projects = [
-  {
-    title: "rené",
-    overview:
-      "rené is a design system for styling clean, simple, lightweight interfaces.",
-    stack: ["Html", "CSS", "Webpack", "npm"],
-    link: "https://renecss.org/",
-    repo: "https://github.com/rnbwdev/rene.css",
-    isSiteLive: true,
-  },
+  // {
+  //   title: "rené",
+  //   overview:
+  //     "rené is a design system for styling clean, simple, lightweight interfaces.",
+  //   stack: ["Html", "CSS", "Webpack", "npm"],
+  //   link: "https://renecss.org/",
+  //   repo: "https://github.com/rnbwdev/rene.css",
+  //   isSiteLive: true,
+  // },
   {
     title: "Dealshare - (Corp Site)",
     overview:
@@ -17,30 +17,30 @@ export const projects = [
     repo: null,
     isSiteLive: true,
   },
-  {
-    title: "Timecounts",
-    overview: "The volunteer organizing platform",
-    stack: ["Html", "React", "Redux", "Sass"],
-    link: "https://join.timecounts.app/",
-    repo: null,
-    isSiteLive: true,
-  },
-  {
-    title: "Samadhan.tech",
-    overview:
-      "Property mapping and management application for a client in India.",
-    stack: ["React", "Redux", "SaSS"],
-    link: "https://samadhan.tech",
-    repo: null,
-    isSiteLive: true,
-  },
-  {
-    title: "Lumian (Archived)",
-    overview:
-      "A platform that summarizes the meeting minutes and action items for the team and allows to query the meeting using AI.",
-    stack: ["React", "Redux", "SaSS"],
-    link: "https://samadhan.tech",
-    repo: null,
-    isSiteLive: true,
-  }
+  // {
+  //   title: "Timecounts",
+  //   overview: "The volunteer organizing platform",
+  //   stack: ["Html", "React", "Redux", "Sass"],
+  //   link: "https://join.timecounts.app/",
+  //   repo: null,
+  //   isSiteLive: true,
+  // },
+  // {
+  //   title: "Samadhan.tech",
+  //   overview:
+  //     "Property mapping and management application for a client in India.",
+  //   stack: ["React", "Redux", "SaSS"],
+  //   link: "https://samadhan.tech",
+  //   repo: null,
+  //   isSiteLive: true,
+  // },
+  // {
+  //   title: "Lumian (Archived)",
+  //   overview:
+  //     "A platform that summarizes the meeting minutes and action items for the team and allows to query the meeting using AI.",
+  //   stack: ["React", "Redux", "SaSS"],
+  //   link: "https://samadhan.tech",
+  //   repo: null,
+  //   isSiteLive: true,
+  // }
 ];
